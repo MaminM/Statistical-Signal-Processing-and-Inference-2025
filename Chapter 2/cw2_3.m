1c4a0c1ab9c4da9b3bab7cafdf59875f391c1044
MaminM <118929434+MaminM@users.noreply.github.com>
2025-03-17 23:50:57 +0000

    fixed cw2_3.m

--- Chapter 2/cw2_3.m
+++ Chapter 2/cw2_3.m
@@ -3,7 +3,7 @@ clc
 close all
 clear all
 
-K = 500; % number of random coeffs
+K = 1000; % number of random coeffs pairs
 a1_list = rand(1,K)*5 - 2.5;
 a2_list = rand(1,K)*3 - 1.5;
 N = 1000; % length of x
@@ -120,6 +120,7 @@ for i = 1:K
         a1_err = [a1_err, a1];
         a2_err = [a2_err, a2];
     end
+
     % true positive
     if theoretical_converge && converge
         a1_true_pos = [a1_true_pos, a1];
@@ -142,52 +143,56 @@ end
 
 
 figure;
-scatter(a2_converge, a1_converge, 'blue', 'o')
+scatter(a1_converge, a2_converge, 'blue', 'o')
 hold on
-scatter(a2_diverge, a1_diverge, 'red', 'o')
-scatter(a2_theory_conv, a1_theory_conv, 'blue', 'x')
-scatter(a2_theory_div, a1_theory_div, 'red', 'x')
-xlabel('a2')
-ylabel('a1')
+scatter(a1_diverge, a2_diverge, 'red', 'o')
+scatter(a1_theory_conv, a2_theory_conv, 'blue', 'x')
+scatter(a1_theory_div, a2_theory_div, 'red', 'x')
+xlabel('a1')
+ylabel('a2')
 legend('Converge', 'Diverge', 'Theoretical Convergence', 'Theoretical Divergence')
 title(['Measured Results: ',title_str])
-
+xlim([-2.5, 2.5])
+ylim([-1.5, 1.5])
 
 figure;
-scatter(a2_theory_conv, a1_theory_conv, 'blue', 'x')
+scatter(a1_theory_conv, a2_theory_conv, 'blue', 'x')
 hold on
-scatter(a2_theory_div, a1_theory_div, 'red', 'x')
-xlabel('a2')
-ylabel('a1')
+scatter(a1_theory_div, a2_theory_div, 'red', 'x')
+xlabel('a1')
+ylabel('a2')
 legend('Converge', 'Diverge')
 title(['Theoretical Convergence Results: ',title_str])
-
+xlim([-2.5, 2.5])
+ylim([-1.5, 1.5])
 
 % plot a figure for when the prediction didn't match the realisation
 gray_hex = '#808080';
 soft_blue_hex = '#87CEEB';
 figure;
-scatter(a2_err, a1_err, 100, '.', 'MarkerEdgeColor', gray_hex)
+scatter(a1_err, a2_err, 100, '.', 'MarkerEdgeColor', gray_hex)
 hold on
-scatter(a2_correct, a1_correct, '.', 'MarkerEdgeColor', soft_blue_hex)
+scatter(a1_correct, a2_correct, '.', 'MarkerEdgeColor', soft_blue_hex)
 legend('Error', 'Correct')
-legend('Converge', 'Diverge')
-xlabel('a2')
-ylabel('a1')
+xlabel('a1')
+ylabel('a2')
 title(['Errors in convergence prediction: ',title_str])
-
+xlim([-2.5, 2.5])
+ylim([-1.5, 1.5])
 
 % plot true/false pos/neg
 figure;
-scatter(a2_true_pos, a1_true_pos, "blue", 'o')
+scatter(a1_true_pos, a2_true_pos, "blue", 'o')
 hold on
-scatter(a2_true_neg, a1_true_neg, "blue", 'x')
-scatter(a2_false_pos, a1_false_pos, 'red', 'o')
-scatter(a2_false_neg, a1_false_neg, 'red', 'x')
+scatter(a1_true_neg, a2_true_neg, "blue", 'x')
+scatter(a1_false_pos, a2_false_pos, 'red', 'o')
+scatter(a1_false_neg, a2_false_neg, 'red', 'x')
 legend('true pos', 'true neg', 'false pos', 'false neg')
-xlabel('a2')
-ylabel('a1')
+xlabel('a1')
+ylabel('a2')
 title(['Errors in convergence prediction: ',title_str])
+xlim([-2.5, 2.5])
+ylim([-1.5, 1.5])
 
 %% Question 2
 %
@@ -249,11 +254,10 @@ clc
 
 load sunspot.dat
 ss_data = sunspot(:, 2);
-ss_data_zero_mean = ss_data - mean(ss_data);
-ss_data_zero_mean_ACF = xcorr(ss_data_zero_mean, 'unbiased');
+ss_data_ACF = xcorr(ss_data, 'unbiased');
 
 % Finding Yule Walker Coefficients
-data = ss_data_zero_mean_ACF;
+data = ss_data_ACF;
 
 MAX_MODEL_ORDER = 10; 
 OFFSET = (length(data) - 1)/2;
@@ -292,37 +296,57 @@ for order_i=1:length(MODEL_ORDERS_TO_PLOT)
     tau = -L:L;
     ACF = ywdata_ACF(-L+OFFSET:L+OFFSET);
     
-    figure;
-    plot(tau,ACF)
-    xlabel('tau')
-    ylabel('ACF')
-    title(['ACF of Yule-Walker | p = ,', int2str(model_order)])
+    % figure;
+    % plot(tau,ACF)
+    % xlabel('tau')
+    % ylabel('ACF')
+    % title(['ACF of Yule-Walker | p = ,', int2str(model_order)])
 end
 
+% Calculate and plot the PSD using FFT of the autocorrelation function
+N = length(ss_data);
+L = 2*N-1; % Length of the ACF
+fs = 1; % Assuming 1 sample per year for sunspot data
 
-% % Manual
-% for p=1:MAX_MODEL_ORDER 
-%     % generate R matrix
-%     R = zeros(p,p);
-%     % test = zeros(p,p);
-%     for rowi=1:p
-%         for coli=1:p
-%             R(rowi,coli) = data(OFFSET + abs(coli - rowi));
-%             % test(rowi, coli) = abs(coli - rowi);
-%         end
-%     end
-% 
-%     % make rxx vector
-%     rxx = zeros(p,1);
-%     for i=1:p
-%         rxx(i,1) = data(OFFSET+i); 
-%     end
-% 
-%     % a = R\rxx;
-%     a = inv(R) * rxx;
-%     a_matrix(p,1:p) = a';
-% 
-% end
+% Compute the PSD using FFT of the ACF (Wiener-Khinchin theorem)
+ss_PSD = fft(ss_data_ACF);
+ss_PSD = abs(ss_PSD(1:floor(L/2)+1));
+ss_PSD = ss_PSD/L; % Normalize by signal length
+
+% % Create frequency vector
+f = (0:floor(L/2))*fs/L;
+% Create normalized angular frequency vector (ω/π)
+omega_norm = f * 2 * pi / fs; % Convert to angular frequency and normalize
+
+% Generate theoretical PSD from YW coefficients
+model_order = 2;
+a = aryule_matrix(model_order, 1:model_order+1);
+Theoretical_PSD = abs(freqz(1,a,N,1)).^ 2 ;
+
+figure
+hold on
+plot(omega_norm, 10*log10(ss_PSD), 'Color', '#000000', 'LineStyle', '--', 'DisplayName', 'Measured PSD');
+for p=1:MAX_MODEL_ORDER
+    a = aryule_matrix(p, 1:p+1);
+    Theoretical_PSD = abs(freqz(1,a,N,1)).^ 2 ;
+    plot(omega_norm, 10*log10(Theoretical_PSD), 'DisplayName', ['Theoretical p = ', int2str(p)]);
+end
+title('Power Spectral Density of Sunspot Data');
+xlabel('Normalized Angular Frequency (ω/π)');
+ylabel('Power/Frequency (dB)');
+xlim([0,pi])
+legend;
+grid on;
+
+
+
+% doing the same on zero mean, unit variance data
+ss_scaled = zscore(ss_data);
+ss_scaled_ACF = xcorr(ss_scaled, 'unbiased');
+
+% calculate PSD
+
+
 
 
 %% plot the PSD of YW overlayed onto data PSD
@@ -379,6 +403,7 @@ ss_PSD = ss_PSD/L; % Normalize by signal length
 
 % % Create frequency vector
 f = (0:floor(L/2))*fs/L;
+
 % 
 % % Plot the PSD
 % figure;
